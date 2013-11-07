84ced48a988203c45536fa7221786879e567c458
McCouman <support@wikibyte.org>
2013-11-07 20:45:22 +0100

    register

--- podwichtel-cms/register/index.php
+++ podwichtel-cms/register/index.php
@@ -1,3 +1,89 @@
+<html>
+<head>
+<title>Podwichteln - Registration</title>
+<style>
+#error { border-left: 3px solid #f00; background: #fee; padding: 20px; color:#a00; }
+#ok { border-left: 3px solid #0a0; background: #efe; padding: 20px; color:#0a0; }
+
+.wmfg_layout_0, table, .wmfg_textarea { font-family: Verdana, Geneva, sans-serif; font-size: 13px; }
+.wmfg_layout_0 ul.wmfg_questions { list-style-type: none; margin: 0; padding: 0; }
+.wmfg_layout_0 ul.wmfg_questions li.wmfg_q { margin: 10px 0; padding: 10px; border: 1px solid #cccccc; }
+.wmfg_layout_0 label.wmfg_label { display: block; margin: 0 0 5px 0; }
+.wmfg_layout_0 table.wmfg_answers { width: 100%; _width: 97%; border-collapse: collapse; }
+.wmfg_layout_0 table.wmfg_answers { vertical-align: top; }
+.wmfg_layout_0 table.wmfg_answers td { padding: 2px; vertical-align: top; }
+.wmfg_layout_0 table.wmfg_answers td.wmfg_a_td { width: 25px; }
+
+.wmfg_layout_0 .wmfg_text { border: 1px solid #CCC; padding: 4px; font-size: 13px; color: #000000; width: 98.5%;
+background-color: #ffffff;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+}
+.wmfg_layout_0 .wmfg_textarea { border: 1px solid #CCC; padding: 4px; font-size: 13px; color: #000000; width: 98.5%;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+background-color: #ffffff;
+}
+.wmfg_layout_0 .wmfg_select { 
+border:1px solid #CCCCCC; padding: 3px; font-size: 13px; color: #000000; margin: 0; width: 100%; _width: 97%;
+background-color: #ffffff;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+}
+.wmfg_layout_0 .wmfg_btn { 
+border: 1px solid #cccccc; cursor: pointer; font-weight: normal; font-size: 13px; padding: 6px; color: #444; font-family: 'lucida grande', tahoma, verdana, arial, sans-serif; 
+background: -webkit-gradient(linear, left top, left bottom, from(#FAFAFA), color-stop(0.5, #FAFAFA), color-stop(0.5, #E5E5E5), to(#F9F9F9)); 
+background: -moz-linear-gradient(top, #FAFAFA, #FAFAFA 50%, #E5E5E5 50%, #F9F9F9);
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr='#FAFAFA', endColorstr='#E5E5E5');
+}
+.wmfg_layout_0 .wmfg_btn:hover {  
+background: -webkit-gradient(linear, left top, left bottom, from(#EDEDED), color-stop(0.5, #EDEDED), color-stop(0.5, #D9D9D9), to(#EDEDED)); 
+background: -moz-linear-gradient(top, #EDEDED, #EDEDED 50%, #D9D9D9 50%, #EDEDED);
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr='#E3326E', endColorstr='#D9D9D9'); 
+</style>
+
+<style>	
+.wmfg_layout_1, table, .wmfg_textarea { font-family: Verdana, Geneva, sans-serif; font-size: 13px; }
+.wmfg_layout_1 ul.wmfg_questions { list-style-type: none; margin: 0; padding: 0; }
+.wmfg_layout_1 ul.wmfg_questions li.wmfg_q { margin: 10px 0; }
+.wmfg_layout_1 label.wmfg_label { display: block; margin: 0 0 5px 0; font-weight:bold; }
+.wmfg_layout_1 table.wmfg_answers { width: 100%; _width: 97%; border-collapse: collapse; }
+.wmfg_layout_1 table.wmfg_answers { vertical-align: top; }
+.wmfg_layout_1 table.wmfg_answers td { padding: 2px; vertical-align: top; }
+.wmfg_layout_1 table.wmfg_answers td.wmfg_a_td { width: 25px; }
+
+.wmfg_layout_1 .wmfg_text { border: 1px solid #CCC; padding: 4px; font-size: 13px; color: #000000; width: 98.5%;
+background-color: #ffffff;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+}
+.wmfg_layout_1 .wmfg_textarea { border: 1px solid #CCC; padding: 4px; font-size: 13px; color: #000000; width: 98.5%;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+background-color: #ffffff;
+}
+.wmfg_layout_1 .wmfg_select { 
+border:1px solid #CCCCCC; padding: 3px; font-size: 13px; color: #000000; margin: 0; width: 100%; _width: 97%;
+background-color: #ffffff;
+background:-webkit-gradient(linear,0 0,0 100%,from(#f8f8f8),to(#fff));
+background:-moz-linear-gradient(top,#f8f8f8,#fff);
+}
+.wmfg_layout_1 .wmfg_btn { 
+border: 1px solid #cccccc; cursor: pointer; font-weight: normal; font-size: 13px; padding: 6px; color: #444; font-family: 'lucida grande', tahoma, verdana, arial, sans-serif; 
+background: -webkit-gradient(linear, left top, left bottom, from(#FAFAFA), color-stop(0.5, #FAFAFA), color-stop(0.5, #E5E5E5), to(#F9F9F9)); 
+background: -moz-linear-gradient(top, #FAFAFA, #FAFAFA 50%, #E5E5E5 50%, #F9F9F9);
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr='#FAFAFA', endColorstr='#E5E5E5');
+}
+.wmfg_layout_1 .wmfg_btn:hover {  
+background: -webkit-gradient(linear, left top, left bottom, from(#EDEDED), color-stop(0.5, #EDEDED), color-stop(0.5, #D9D9D9), to(#EDEDED)); 
+background: -moz-linear-gradient(top, #EDEDED, #EDEDED 50%, #D9D9D9 50%, #EDEDED);
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr='#E3326E', endColorstr='#D9D9D9'); 
+</style>
+
+
+</head>
+<body>
+<h2>Podwichteln - Registration</h2>
 <?php
 $verbindung = mysql_connect ('localhost', 'root', 'root')
 or die ("Keine Verbindung zur Datenbank m&ouml;glich. Benutzername oder Passwort sind falsch!");
@@ -55,57 +141,76 @@ if(htmlspecialchars($_POST['mail']) != '') {
 				VALUES 
 				('$date', '$key', '$podname', '$podurl', '$ziplink', '$username', '$mail', '$wichtel')"); 
 	
-			echo 'Erfolgreich Angemeldet!';
+			echo '<div id="ok">Erfolgreich Angemeldet!</div>';
 	
-		} else { echo 'Error: Speichern der Daten'; }
+		} else { echo '<div id="error">Error: Speichern der Daten</div>'; }
 		
 
 ########## Abfragen ###################
 
-	} else { echo 'Du hast mehrere Podcasts?'; } //Mail
+	} else { echo '<div id="error">Du hast mehrere Podcasts?</div>'; } //Mail
 
-} else { echo 'Wir benötigen schon eine E-Mail von dir, damit wir dir antworten können!'; } //!Mail
+} else { echo '<div id="error">Wir benötigen schon eine E-Mail von dir, damit wir dir antworten können!</div>'; } //!Mail
 
-	} else { echo 'Aber wir haben doch schon eine Zipfile von dir?'; } //Zip
+	} else { echo '<div id="error">Aber wir haben doch schon eine Zipfile von dir?</div>'; } //Zip
 
-} else { echo 'Du hast vergessen einen Link zu deinem Zipfile einzugeben!'; } //!Zip
+} else { echo '<div id="error">Du hast vergessen einen Link zu deinem Zipfile einzugeben!</div>'; } //!Zip
 
-	} else { echo 'Du hast deinen Podcast schon eingereicht!'; } //podcast Link
+	} else { echo '<div id="error">Du hast deinen Podcast schon eingereicht!</div>'; } //podcast Link
 
-} else { echo 'Du hast vergessen einen Link zu deinem Podcast einzugeben!'; } //!Podcast url
+} else { echo '<div id="error">Du hast vergessen einen Link zu deinem Podcast einzugeben!</div>'; } //!Podcast url
 
-	} else { echo 'Du hast deinen Podcast schon eingereicht!'; } //podcast Name
+	} else { echo '<div id="error">Du hast deinen Podcast schon eingereicht!</div>'; } //podcast Name
 
-} else { echo 'Du hast vergessen den Namen deines Podcasts einzugeben!'; } //!Podcast Name
+} else { echo '<div id="error">Du hast vergessen den Namen deines Podcasts einzugeben!</div>'; } //!Podcast Name
 
-	} else { echo 'Du hast dich schon angemeldet!'; } //Username	
+	} else { echo '<div id="error">Du hast dich schon angemeldet!</div>'; } //Username	
 
-} else { echo 'Du hast vergessen deinen Namen einzugeben!'; } //!Unsername
+} else { echo '<div id="error">Du hast vergessen deinen Namen einzugeben!</div>'; } //!Unsername
 
 //End
 }
 
+?>
+<div style="width:600px" class="wmfg_layout_0">
 
-?> 
-<form action="register.php" method="post"> 
+<form method="post" action="register.php">
 
-  
-<label>Username</label><br>
-<input type="text" name="username" value="<?php echo htmlspecialchars($username); ?>" size="20" maxlength="25" />  <br /> 
+<ul class="wmfg_questions">
 
-<label>Podcast URL</label><br>
-<input type="text" name="podurl" value="<?php echo htmlspecialchars($podurl); ?>" size="20" maxlength="25" />  <br /> 
+	<li class="wmfg_q">
+		<label class="wmfg_label" for="uname">Vorname, Nachname</label>
+		<input placeholder="Max Mustermann" type="text" class="wmfg_text" id="uname" name="username" value="<?php echo htmlspecialchars($username); ?>" />
+	</li>
 
-<label>Podcast Name</label><br>
-<input type="text" name="podname" value="<?php echo htmlspecialchars($podname); ?>" size="20" maxlength="25" />  <br /> 
+	<li class="wmfg_q">
+		<label class="wmfg_label" for="pname">Podcast Name</label>
+		<input placeholder="Coolpod" type="text" class="wmfg_text" id="pname" name="podname" value="<?php echo htmlspecialchars($podname); ?>" />
+	</li>
 
-<label>Zip URL</label><br>
-<input type="text" name="ziplink" value="<?php echo htmlspecialchars($ziplink); ?>" size="20" maxlength="25" />  <br /> 
+	<li class="wmfg_q">
+		<label class="wmfg_label" for="purl">Podcast URL</label>
+		<input placeholder="http://coolpod.de" type="text" class="wmfg_text" id="purl" name="podurl" value="<?php echo htmlspecialchars($podurl); ?>" />
+	</li>
 
-<label>Email</label><br>
-<input type="text" name="mail" value="<?php echo htmlspecialchars($mail); ?>" size="20" maxlength="25" />  <br /> 
+	<li class="wmfg_q">
+		<label class="wmfg_label" for="zurl">Zip URL</label>
+		<input placeholder="http://coolpod.de/coolpod-intro.zip" type="text" class="wmfg_text" id="zurl" name="ziplink" value="<?php echo htmlspecialchars($ziplink); ?>"  />
+	</li>
 
-<br>
-<input type="Submit" name="submit" value="Register" /> 
+	<li class="wmfg_q">
+		<label class="wmfg_label" for="email">E-Mail</label>
+		<input placeholder="info@coolpod.de" type="text" class="wmfg_text" id="email" name="mail" name="mail" value="<?php echo htmlspecialchars($mail); ?>"  />
+	</li>
+
+
+	<li class="wmfg_q">
+		<input type="submit" class="wmfg_btn" name="submit" id="submit_id" value="Register" />
+	</li>
+</ul>
 
 </form>
+
+</div>
+</body>
+</html>
